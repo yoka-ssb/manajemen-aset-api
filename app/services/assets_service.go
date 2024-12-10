6b9d3d4297e179d57a007f3556c4f40edc261590
Indera Jati <it.software@ssbhjhestiwidodo.com>
2024-12-10 11:33:59 +0700

    Asset API: remove personal_responsible_id

--- app/services/assets_service.go
+++ app/services/assets_service.go
@@ -143,10 +143,9 @@ func (s *AssetService) GetAsset(ctx context.Context, req *assetpb.GetAssetReques
 	log.Default().Println("getting asset with ID: ", req.GetId())
 	var asset assetpb.Asset
 
-	query := db.Select("assets.*, maintenance_periods.period_name AS maintenance_period_name, areas.area_name AS area_name, outlets.outlet_name AS outlet_name, personal_responsibles.personal_name AS personal_name, roles.role_name AS asset_pic_name, classifications.classification_name AS asset_classification_name, EXTRACT(MONTH FROM AGE(CURRENT_DATE, assets.asset_purchase_date)) AS asset_age").
+	query := db.Select("assets.*, maintenance_periods.period_name AS maintenance_period_name, areas.area_name AS area_name, outlets.outlet_name AS outlet_name, roles.role_name AS asset_pic_name, classifications.classification_name AS asset_classification_name, EXTRACT(MONTH FROM AGE(CURRENT_DATE, assets.asset_purchase_date)) AS asset_age").
 		Joins("LEFT JOIN areas ON assets.area_id = areas.area_id").
 		Joins("LEFT JOIN outlets ON assets.outlet_id = outlets.outlet_id").
-		Joins("LEFT JOIN personal_responsibles ON assets.personal_responsible_id = personal_responsibles.personal_id").
 		Joins("LEFT JOIN roles ON assets.asset_pic = roles.role_id").
 		Joins("LEFT JOIN classifications ON assets.asset_classification = classifications.classification_id").
 		Joins("LEFT JOIN maintenance_periods ON classifications.maintenance_period_id = maintenance_periods.period_id").
@@ -172,10 +171,9 @@ func (s *AssetService) GetAssetByHash(ctx context.Context, req *assetpb.GetAsset
 	log.Default().Println("getting asset by hash ID: ", req.GetHashId())
 	var asset assetpb.Asset
 
-	query := db.Select("assets.*, areas.area_name AS area_name, outlets.outlet_name AS outlet_name, personal_responsibles.personal_name AS personal_name, roles.role_name AS asset_pic_name, classifications.classification_name AS asset_classification_name, EXTRACT(MONTH FROM AGE(CURRENT_DATE, assets.asset_purchase_date)) AS asset_age").
+	query := db.Select("assets.*, areas.area_name AS area_name, outlets.outlet_name AS outlet_name, roles.role_name AS asset_pic_name, classifications.classification_name AS asset_classification_name, EXTRACT(MONTH FROM AGE(CURRENT_DATE, assets.asset_purchase_date)) AS asset_age").
 		Joins("LEFT JOIN areas ON assets.area_id = areas.area_id").
 		Joins("LEFT JOIN outlets ON assets.outlet_id = outlets.outlet_id").
-		Joins("LEFT JOIN personal_responsibles ON assets.personal_responsible_id = personal_responsibles.personal_id").
 		Joins("LEFT JOIN roles ON assets.asset_pic = roles.role_id").
 		Joins("LEFT JOIN classifications ON assets.asset_classification = classifications.classification_id").
 		Where("assets.asset_id_hash = ?", req.GetHashId())
@@ -345,7 +343,7 @@ func (s *AssetService) ListAssets(ctx context.Context, req *assetpb.ListAssetsRe
 func getAssets(offset, limit int32, q string) ([]*assetpb.Asset, error) {
 	// Query the database to get the assets
 	var assets []*assetpb.Asset
-	query := db.Select("assets.*, areas.area_name AS area_name, outlets.outlet_name AS outlet_name, personal_responsibles.personal_name AS personal_name").
+	query := db.Select("assets.*, areas.area_name AS area_name, outlets.outlet_name AS outlet_name").
 		Limit(int(limit)).Offset(int(offset))
 
 	if q != "" {
@@ -355,8 +353,7 @@ func getAssets(offset, limit int32, q string) ([]*assetpb.Asset, error) {
 	// get data area name, outlet name, personal name
 	query = query.
 		Joins("LEFT JOIN areas ON assets.area_id = areas.area_id").
-		Joins("LEFT JOIN outlets ON assets.outlet_id = outlets.outlet_id").
-		Joins("LEFT JOIN personal_responsibles ON assets.personal_responsible_id = personal_responsibles.personal_id")
+		Joins("LEFT JOIN outlets ON assets.outlet_id = outlets.outlet_id")
 
 	err := query.Find(&assets).Error
 	if err != nil {
@@ -369,10 +366,9 @@ func getAssets(offset, limit int32, q string) ([]*assetpb.Asset, error) {
 
 func GetAssetById(id int32) (*assetpb.Asset, error) {
 	var asset assetpb.Asset
-	query := db.Select("assets.*, maintenance_periods.period_name AS maintenance_period_name, areas.area_name AS area_name, outlets.outlet_name AS outlet_name, personal_responsibles.personal_name AS personal_name, roles.role_name AS asset_pic_name, classifications.classification_name AS asset_classification_name, EXTRACT(MONTH FROM AGE(CURRENT_DATE, assets.asset_purchase_date)) AS asset_age").
+	query := db.Select("assets.*, maintenance_periods.period_name AS maintenance_period_name, areas.area_name AS area_name, outlets.outlet_name AS outlet_name, roles.role_name AS asset_pic_name, classifications.classification_name AS asset_classification_name, EXTRACT(MONTH FROM AGE(CURRENT_DATE, assets.asset_purchase_date)) AS asset_age").
 		Joins("LEFT JOIN areas ON assets.area_id = areas.area_id").
 		Joins("LEFT JOIN outlets ON assets.outlet_id = outlets.outlet_id").
-		Joins("LEFT JOIN personal_responsibles ON assets.personal_responsible_id = personal_responsibles.personal_id").
 		Joins("LEFT JOIN roles ON assets.asset_pic = roles.role_id").
 		Joins("LEFT JOIN classifications ON assets.asset_classification = classifications.classification_id").
 		Joins("LEFT JOIN maintenance_periods ON classifications.maintenance_period_id = maintenance_periods.period_id").
