b993e6d6196f0a0d830b488f533a31ad6e534042
it-softwareengineerSSB <it.software@ssbhjhestiwidodo.com>
2025-02-26 15:37:07 +0700

    fix: bug maintenance period service

--- app/services/maintenance_period_service.go
+++ app/services/maintenance_period_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"asset-management-api/assetpb"
 	"context"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog/log"
@@ -23,11 +24,36 @@ func (s *MaintenancePeriodService) Register(server interface{}) {
 	assetpb.RegisterMAINTENANCEPERIODServiceServer(grpcServer, s)
 }
 
+func (s *MaintenancePeriodService) CreateMaintenancePeriod(ctx context.Context, req *assetpb.CreateMaintenancePeriodRequest) (*assetpb.CreateMaintenancePeriodResponse, error) {
+	log.Info().Msg("Creating maintenance period")
+
+	query := `
+    INSERT INTO maintenance_periods (period_name, maintenance_date)
+    VALUES ($1, $2)
+    RETURNING period_id
+`
+	var periodID int
+	err := s.DB.QueryRow(ctx, query, req.GetPeriodName(), req.GetMaintenanceDate()).Scan(&periodID)
+
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to create maintenance period")
+		return &assetpb.CreateMaintenancePeriodResponse{
+			Message: "Error creating maintenance period",
+			Code:    "500",
+		}, nil
+	}
+
+	return &assetpb.CreateMaintenancePeriodResponse{
+		Message: "Success",
+		Code:    "200",
+	}, nil
+}
+
 func (s *MaintenancePeriodService) ListMaintenancePeriod(ctx context.Context, req *assetpb.ListMaintenancePeriodRequest) (*assetpb.ListMaintenancePeriodResponse, error) {
 	log.Info().Msg("Fetching maintenance periods")
 
 	query := `
-        SELECT id, period_name, maintenance_date
+        SELECT period_id, period_name, maintenance_date
         FROM maintenance_periods
     `
 	rows, err := s.DB.Query(ctx, query)
@@ -45,15 +71,21 @@ func (s *MaintenancePeriodService) ListMaintenancePeriod(ctx context.Context, re
 
 	for rows.Next() {
 		var period assetpb.MaintenancePeriod
+		var maintenanceDate time.Time
+
 		err := rows.Scan(
 			&period.PeriodId,
 			&period.PeriodName,
-			&period.MaintenanceDate,
+			&maintenanceDate,
 		)
 		if err != nil {
 			log.Error().Err(err).Msg("Failed to scan maintenance period row")
 			continue
 		}
+
+		// Konversi time.Time ke string dalam format YYYY-MM-DD
+		period.MaintenanceDate = maintenanceDate.Format("2006-01-02")
+
 		maintenancePeriods = append(maintenancePeriods, &period)
 	}
 
@@ -63,29 +95,6 @@ func (s *MaintenancePeriodService) ListMaintenancePeriod(ctx context.Context, re
 
 	return &assetpb.ListMaintenancePeriodResponse{
 		Data:    maintenancePeriods,
-		Message: "Success",
-		Code:    "200",
-	}, nil
-}
-
-func (s *MaintenancePeriodService) CreateMaintenancePeriod(ctx context.Context, req *assetpb.CreateMaintenancePeriodRequest) (*assetpb.CreateMaintenancePeriodResponse, error) {
-	log.Info().Msg("Creating maintenance period")
-
-	query := `
-        INSERT INTO maintenance_periods (period_name, maintenance_date)
-        VALUES ($1, $2)
-    `
-	_, err := s.DB.Exec(ctx, query, req.GetPeriodName(), req.GetMaintenanceDate())
-
-	if err != nil {
-		log.Error().Err(err).Msg("Failed to create maintenance period")
-		return &assetpb.CreateMaintenancePeriodResponse{
-			Message: "Error creating maintenance period",
-			Code:    "500",
-		}, nil
-	}
-
-	return &assetpb.CreateMaintenancePeriodResponse{
 		Message: "Success",
 		Code:    "200",
 	}, nil
