dbfe5c0e96f0a127bff06e93c661182b762223c4
it-softwareengineerSSB <it.software@ssbhjhestiwidodo.com>
2025-02-26 11:31:20 +0700

    fix: orm + pgxpool

--- app/database/db.go
+++ app/database/db.go
@@ -1,34 +1,56 @@
 package database
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"os"
+	"time"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/rs/zerolog/log"
 )
 
-func DBConn() *gorm.DB {
-	var db *gorm.DB
+func DBConn() *pgxpool.Pool {
+	// Load environment variables
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal().Err(err).Msg("Error loading .env file")
 	}
 
+	// Format DSN
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
 	)
 
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	log.Info().Msg("Connecting to database...")
+
+	// Configure connection pool
+	config, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
-		log.Fatal("Error connecting to database")
+		log.Fatal().Err(err).Msg("Unable to parse database configuration")
 	}
-	return db
+
+	// Create connection pool
+	dbpool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Unable to create connection pool")
+	}
+
+	// Check database connection
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = dbpool.Ping(ctx)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Database connection is not alive")
+	}
+
+	log.Info().Msg("Successfully connected to database!")
+	return dbpool
 }
