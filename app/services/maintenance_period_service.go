c62237dc02b3927dc9c4e247ba71f36bd9b0545f
it-softwareengineerSSB <it.software@ssbhjhestiwidodo.com>
2025-02-26 15:26:31 +0700

    fix: orm maintenance period service + pgxpool

--- app/services/maintenance_period_service.go
+++ app/services/maintenance_period_service.go
@@ -3,20 +3,19 @@ package services
 import (
 	"asset-management-api/assetpb"
 	"context"
-	"github.com/rs/zerolog/log"
 
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
-	"gorm.io/gorm"
 )
 
 type MaintenancePeriodService struct {
-	MasterService
+	DB *pgxpool.Pool
 	assetpb.UnimplementedMAINTENANCEPERIODServiceServer
 }
 
-func NewMaintenancePeriodService(db *gorm.DB) *MaintenancePeriodService {
-	return &MaintenancePeriodService{
-		MasterService: MasterService{DB: db}}
+func NewMaintenancePeriodService(db *pgxpool.Pool) *MaintenancePeriodService {
+	return &MaintenancePeriodService{DB: db}
 }
 
 func (s *MaintenancePeriodService) Register(server interface{}) {
@@ -25,40 +24,69 @@ func (s *MaintenancePeriodService) Register(server interface{}) {
 }
 
 func (s *MaintenancePeriodService) ListMaintenancePeriod(ctx context.Context, req *assetpb.ListMaintenancePeriodRequest) (*assetpb.ListMaintenancePeriodResponse, error) {
-	log.Info().Msg("List maintenance period")
-	var maintenancePeriods []*assetpb.MaintenancePeriod
-	result := db.Find(&maintenancePeriods)
-	if result.Error != nil {
+	log.Info().Msg("Fetching maintenance periods")
+
+	query := `
+        SELECT id, period_name, maintenance_date
+        FROM maintenance_periods
+    `
+	rows, err := s.DB.Query(ctx, query)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to fetch maintenance periods")
 		return &assetpb.ListMaintenancePeriodResponse{
-			Data : nil,
+			Data:    nil,
 			Message: "Error fetching data",
-			Code: "500",
-			}, nil
-	}
-	return &assetpb.ListMaintenancePeriodResponse{
-		Data: maintenancePeriods,
-		Message: "Success",
-		Code: "200",
+			Code:    "500",
 		}, nil
+	}
+	defer rows.Close()
+
+	var maintenancePeriods []*assetpb.MaintenancePeriod
+
+	for rows.Next() {
+		var period assetpb.MaintenancePeriod
+		err := rows.Scan(
+			&period.PeriodId,
+			&period.PeriodName,
+			&period.MaintenanceDate,
+		)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to scan maintenance period row")
+			continue
+		}
+		maintenancePeriods = append(maintenancePeriods, &period)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Error().Err(err).Msg("Error iterating maintenance period rows")
+	}
+
+	return &assetpb.ListMaintenancePeriodResponse{
+		Data:    maintenancePeriods,
+		Message: "Success",
+		Code:    "200",
+	}, nil
 }
 
 func (s *MaintenancePeriodService) CreateMaintenancePeriod(ctx context.Context, req *assetpb.CreateMaintenancePeriodRequest) (*assetpb.CreateMaintenancePeriodResponse, error) {
-	log.Info().Msg("Create maintenance period")
+	log.Info().Msg("Creating maintenance period")
 
-	maintenancePeriod := map[string]interface{}{
-		"PeriodName": req.GetPeriodName(),
-		"MaintenanceDate": req.GetMaintenanceDate(),
-	}
-	result := db.Model(&assetpb.MaintenancePeriod{}).Create(&maintenancePeriod)
-	if result.Error != nil {
+	query := `
+        INSERT INTO maintenance_periods (period_name, maintenance_date)
+        VALUES ($1, $2)
+    `
+	_, err := s.DB.Exec(ctx, query, req.GetPeriodName(), req.GetMaintenanceDate())
+
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to create maintenance period")
 		return &assetpb.CreateMaintenancePeriodResponse{
 			Message: "Error creating maintenance period",
-			Code: "500",
-			}, nil
+			Code:    "500",
+		}, nil
 	}
 
 	return &assetpb.CreateMaintenancePeriodResponse{
 		Message: "Success",
-		Code: "200",
+		Code:    "200",
 	}, nil
 }
