5150049771ab82e93a58c9a04de9df1a192ca93b
it-softwareengineerSSB <it.software@ssbhjhestiwidodo.com>
2025-02-26 11:40:14 +0700

    orm outlet service + pgxpool

--- app/services/outlet_service.go
+++ app/services/outlet_service.go
@@ -1,21 +1,26 @@
 package services
 
 import (
-    "asset-management-api/assetpb"
-    "context"
-    "github.com/rs/zerolog/log"
-    "google.golang.org/grpc"
-    "gorm.io/gorm"
+	"asset-management-api/assetpb"
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
 )
 
 type OutletService struct {
     MasterService
     assetpb.UnimplementedOUTLETServiceServer
+    DB *pgxpool.Pool
 }
 
-func NewOutletService(db *gorm.DB) *OutletService {
+func NewOutletService(db *pgxpool.Pool) *OutletService {
     return &OutletService{
-        MasterService: MasterService{DB: db}}
+        MasterService: MasterService{DB: db},
+        DB:            db,
+    }
 }
 
 func (s *OutletService) Register(server interface{}) {
@@ -26,73 +31,69 @@ func (s *OutletService) Register(server interface{}) {
 func (s *OutletService) ListOutlet(ctx context.Context, req *assetpb.ListOutletRequest) (*assetpb.ListOutletResponse, error) {
     log.Info().Msg("List outlet")
     var outlets []*assetpb.Outlet
-    var query = db
-    
-    // Find outlet by area ID
+    var rows pgx.Rows
+    var err error
+
     if req.AreaId != 0 {
         log.Info().Msgf("List outlet with area ID: %d", req.AreaId)
-        query = query.Joins("JOIN area_outlets ON outlets.outlet_id = area_outlets.outlet_id").
-        Where("area_outlets.area_id = ?", req.AreaId)
+        query := "SELECT o.outlet_id, o.outlet_name FROM outlets o JOIN area_outlets ao ON o.outlet_id = ao.outlet_id WHERE ao.area_id = $1"
+        rows, err = s.DB.Query(ctx, query, req.AreaId)
+    } else {
+        query := "SELECT outlet_id, outlet_name FROM outlets"
+        rows, err = s.DB.Query(ctx, query)
     }
 
-    result := query.Find(&outlets)
-    if result.Error != nil {
-        log.Error().Err(result.Error).Msg("Error fetching data")
+    if err != nil {
+        log.Error().Err(err).Msgf("Error fetching data for area_id: %d", req.AreaId)
         return &assetpb.ListOutletResponse{
-            Data: nil,
-            Message: "Error fetching data",
-            Code: "500",
+            Data:    nil,
+            Message: "Error fetching data: " + err.Error(),
+            Code:    "500",
         }, nil
     }
+
+    defer rows.Close()
+
+    for rows.Next() {
+        var outlet assetpb.Outlet
+        if err := rows.Scan(&outlet.OutletId, &outlet.OutletName); err != nil {
+            log.Error().Err(err).Msg("Error scanning row")
+            continue
+        }
+        outlets = append(outlets, &outlet)
+    }
+
     return &assetpb.ListOutletResponse{
-        Data: outlets,
+        Data:    outlets,
         Message: "Success",
-        Code: "200",
+        Code:    "200",
     }, nil
 }
 
 func (s *OutletService) CreateOutlet(ctx context.Context, req *assetpb.CreateOutletRequest) (*assetpb.CreateOutletResponse, error) {
-    outlet := map[string]interface{}{
-        "OutletName": req.GetOutletName(),
-    }
-
-    // Create data outlet
-    outletResult := db.Model(&assetpb.Outlet{}).Create(&outlet)
-    if outletResult.Error != nil {
-        log.Error().Err(outletResult.Error).Msg("Error creating data outlet")
+    query := "INSERT INTO outlets (outlet_name) VALUES ($1) RETURNING outlet_id"
+    var outletId int64
+    err := s.DB.QueryRow(ctx, query, req.GetOutletName()).Scan(&outletId)
+    if err != nil {
+        log.Error().Err(err).Msg("Error creating data outlet")
         return &assetpb.CreateOutletResponse{
             Message: "Error creating data outlet",
-            Code: "500",
+            Code:    "500",
         }, nil
     }
 
-    // Get outlet ID
-    var dataOutlet assetpb.Outlet 
-    getOutletId := db.Model(&assetpb.Outlet{}).Where("outlet_name = ?", req.GetOutletName()).Find(&dataOutlet)
-    if getOutletId.Error != nil {
-        log.Error().Err(getOutletId.Error).Msg("Error fetching data outlet")
-        return &assetpb.CreateOutletResponse{
-            Message: "Error fetching data outlet",
-            Code: "500",
-        }, nil
-    }
-
-    areaOutlet := assetpb.AreaOutlet{
-        AreaId: req.GetAreaId(),
-        OutletId: dataOutlet.OutletId,
-    }
-    log.Info().Msgf("Outlet ID: %d", dataOutlet.OutletId)
-    areaOutletResult := db.Model(&assetpb.AreaOutlet{}).Create(&areaOutlet)
-    if areaOutletResult.Error != nil {
-        log.Error().Err(areaOutletResult.Error).Msg("Error creating data area outlet")
+    areaQuery := "INSERT INTO area_outlets (area_id, outlet_id) VALUES ($1, $2)"
+    _, err = s.DB.Exec(ctx, areaQuery, req.GetAreaId(), outletId)
+    if err != nil {
+        log.Error().Err(err).Msg("Error creating data area outlet")
         return &assetpb.CreateOutletResponse{
             Message: "Error creating data area outlet",
-            Code: "500",
+            Code:    "500",
         }, nil
     }
 
     return &assetpb.CreateOutletResponse{
         Message: "Success",
-        Code: "200",
+        Code:    "200",
     }, nil
 }
